dabcf96e54bde4670d639e3f465b53a59b56dba8
Daniel Tobon <daniel_tobon@epam.com>
2023-10-27 15:08:00 -0700

    Added dependency injection with simple injector library

--- FileSystemTask/FileSystemVisitor.cs
+++ FileSystemTask/FileSystemVisitor.cs
@@ -6,6 +6,56 @@ using System.Threading.Tasks;
 
 namespace FileSystemTask
 {
+    //public class FileSystemVisitor
+    //{
+    //    private readonly string _rootDirectory;
+    //    private readonly Func<FileSystemInfo, bool> _filter;
+
+    //    public FileSystemVisitor(string rootDirectory)
+    //    {
+    //        _rootDirectory = rootDirectory;
+    //        _filter = null;
+    //    }
+
+    //    public FileSystemVisitor(string rootDirectory, Func<FileSystemInfo, bool> filter)
+    //    {
+    //        _rootDirectory = rootDirectory;
+    //        _filter = filter;
+    //    }
+
+    //    public IEnumerable<FileSystemInfo> Traverse()
+    //    {
+    //        var queue = new Queue<DirectoryInfo>();
+    //        queue.Enqueue(new DirectoryInfo(_rootDirectory));
+
+    //        while (queue.Count > 0)
+    //        {
+    //            var directory = queue.Dequeue();
+
+    //            foreach (var file in directory.GetFiles())
+    //            {
+    //                if (_filter != null && !_filter(file))
+    //                {
+    //                    continue;
+    //                }
+
+    //                yield return file;
+    //            }
+
+    //            foreach (var subdirectory in directory.GetDirectories())
+    //            {
+    //                if (_filter != null && !_filter(subdirectory))
+    //                {
+    //                    continue;
+    //                }
+
+    //                yield return subdirectory;
+
+    //                queue.Enqueue(subdirectory);
+    //            }
+    //        }
+    //    }
+    //}
     public class FileSystemVisitor
     {
         private readonly string _rootDirectory;
@@ -23,10 +73,12 @@ namespace FileSystemTask
             _filter = filter;
         }
 
+        public string RootDirectory { get; set; }
+
         public IEnumerable<FileSystemInfo> Traverse()
         {
             var queue = new Queue<DirectoryInfo>();
-            queue.Enqueue(new DirectoryInfo(_rootDirectory));
+            queue.Enqueue(new DirectoryInfo(RootDirectory));
 
             while (queue.Count > 0)
             {
@@ -57,3 +109,4 @@ namespace FileSystemTask
         }
     }
 }
+
--- FileSystemTask/Program.cs
+++ FileSystemTask/Program.cs
@@ -1,10 +1,23 @@
 ﻿using FileSystemTask;
+using SimpleInjector;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var fileSystemVisitor = new FileSystemVisitor(args[0]);
+        //var fileSystemVisitor = new FileSystemVisitor(args[0]);
+
+        //foreach (var fileSystemInfo in fileSystemVisitor.Traverse())
+        //{
+        //    Console.WriteLine(fileSystemInfo.FullName);
+        //}
+        var container = new Container();
+
+        container.Register<FileSystemVisitor>(() => new FileSystemVisitor(""));
+
+        var fileSystemVisitor = container.GetInstance<FileSystemVisitor>();
+
+        fileSystemVisitor.RootDirectory = args[0];
 
         foreach (var fileSystemInfo in fileSystemVisitor.Traverse())
         {
