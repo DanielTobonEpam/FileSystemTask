21ad671374cd5b87a9d1ffc542eaacca38d4bb4c
Daniel_Tobon <Daniel_Tobon@EPMXGUAW2057>
2023-10-27 13:51:59 -0700

    Add project files.

---
+++ FileSystemTask/FileSystemVisitor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemTask
+{
+    public class FileSystemVisitor
+    {
+        private readonly string _rootDirectory;
+        private readonly Func<FileSystemInfo, bool> _filter;
+
+        public FileSystemVisitor(string rootDirectory)
+        {
+            _rootDirectory = rootDirectory;
+            _filter = null;
+        }
+
+        public FileSystemVisitor(string rootDirectory, Func<FileSystemInfo, bool> filter)
+        {
+            _rootDirectory = rootDirectory;
+            _filter = filter;
+        }
+
+        public IEnumerable<FileSystemInfo> Traverse()
+        {
+            var queue = new Queue<DirectoryInfo>();
+            queue.Enqueue(new DirectoryInfo(_rootDirectory));
+
+            while (queue.Count > 0)
+            {
+                var directory = queue.Dequeue();
+
+                foreach (var file in directory.GetFiles())
+                {
+                    if (_filter != null && !_filter(file))
+                    {
+                        continue;
+                    }
+
+                    yield return file;
+                }
+
+                foreach (var subdirectory in directory.GetDirectories())
+                {
+                    if (_filter != null && !_filter(subdirectory))
+                    {
+                        continue;
+                    }
+
+                    yield return subdirectory;
+
+                    queue.Enqueue(subdirectory);
+                }
+            }
+        }
+    }
+}
---
+++ FileSystemTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using FileSystemTask;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileSystemVisitor = new FileSystemVisitor(args[0]);
+
+        foreach (var fileSystemInfo in fileSystemVisitor.Traverse())
+        {
+            Console.WriteLine(fileSystemInfo.FullName);
+        }
+    }
+}
